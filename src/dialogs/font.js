80a22f580221584bd35053c81dbc16badebe7003
Aaron Meese <ajmeese7@gmail.com>
2022-03-28 03:39:00 +0100

    Fixed typo
    Updated "chose" to "choose"

--- src/dialogs/font.js
+++ src/dialogs/font.js
@@ -57,7 +57,7 @@ export default class FontDialog extends Dialog {
    */
   constructor(core, args, callback) {
     args = Object.assign({}, {
-      title: 'Chose Font',
+      title: 'Choose Font',
       minSize: 6,
       maxSize: 48,
       unit: 'px',
